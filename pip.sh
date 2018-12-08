caed1dd0c255eb330332c775465ed4ba1acfd452
root <root@ip-172-31-82-54.ec2.internal>
2018-12-08 02:21:10 +0000

    consul server

---
+++ pip.sh
@@ -0,0 +1,8 @@
+yum install wget -y
+wget https://pypi.python.org/packages/source/s/setuptools/setuptools-7.0.tar.gz --no-check-certificate
+tar xzf setuptools-7.0.tar.gz
+cd setuptools-7.0
+python setup.py install
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+pip install ansible
